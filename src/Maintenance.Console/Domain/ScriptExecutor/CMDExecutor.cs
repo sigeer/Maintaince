29e7676e39b80799eecfd41fc56bb0b46fb5a8bb
sigeer <1452069418@qq.com>
2024-03-08 17:00:07 +0800

    修复：执行cmd的工作目录替换

--- src/Maintenance.Console/Domain/ScriptExecutor/CMDExecutor.cs
+++ src/Maintenance.Console/Domain/ScriptExecutor/CMDExecutor.cs
@@ -4,21 +4,26 @@ namespace Maintenance.Console.Domain.ScriptExecutor
 {
     internal class CMDExecutor
     {
-        public static string Run(string command)
+        public static string Run(string batPath)
         {
-            command = command.Replace("{{WORKDIR}}", Environment.CurrentDirectory);
-
             Process process = new Process();
-            process.StartInfo.WorkingDirectory = Path.GetDirectoryName(command);
+            process.StartInfo.WorkingDirectory = Path.GetDirectoryName(batPath);
             process.StartInfo.FileName = "cmd.exe"; // 指定要执行的命令解释器（这里是 cmd）
-            process.StartInfo.Arguments = $"/c {command}"; // 指定要执行的命令及参数，/c 表示执行完命令后关闭 cmd 窗口
             process.StartInfo.UseShellExecute = false; // 不使用操作系统外壳程序启动进程
             process.StartInfo.RedirectStandardOutput = true; // 重定向标准输出，以便从进程读取输出信息
+            process.StartInfo.RedirectStandardInput = true;
 
-            process.Start(); // 启动进程
+            process.Start();
+
+            process.StandardInput.WriteLine($"set WORKDIR={Environment.CurrentDirectory}");
+            process.StandardInput.Flush();
+
+            process.StandardInput.WriteLine(batPath);
+            process.StandardInput.Flush();
 
             // 读取进程的输出
             string output = process.StandardOutput.ReadToEnd();
+            var err = process.StandardError.ReadToEnd();
 
             process.WaitForExit(); // 等待进程执行完成
             process.Close(); // 关闭进程
